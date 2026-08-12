059907f5ec2e713b076b25f0c51fcdce718d32aa
charliemarji1-web <charliemarji1@gmail.com>
2026-08-12 10:22:00 -0400

    Create Workouts.js

---
+++ Workouts.js
@@ -0,0 +1,77 @@
+const switch_page = document.querySelector(".try_once");
+
+if (switch_page) {
+    switch_page.addEventListener("click", () => {
+        window.location.href = "Abs_exercise.html";
+    });
+}
+
+
+const switch_page2 = document.querySelector(".try_twice");
+
+if (switch_page2) {
+    switch_page2.addEventListener("click", () => {
+        window.location.href = "Chest_exercise.html";
+    });
+}
+
+
+const switch_page3 = document.querySelector(".try_three");
+
+if (switch_page3) {
+    switch_page3.addEventListener("click", () => {
+        window.location.href = "Leg_exercise.html";
+    });
+}
+
+
+const switch_page4 = document.querySelector(".try_four");
+
+if (switch_page4) {
+    switch_page4.addEventListener("click", () => {
+        window.location.href = "Back_exercise.html";
+    });
+}
+
+
+const switch_page5 = document.querySelector(".try_five");
+
+if (switch_page5) {
+    switch_page5.addEventListener("click", () => {
+        window.location.href = "Arm_exercise.html";
+    });
+}
+
+
+const switch_page6 = document.querySelector(".try_six");
+
+if (switch_page6) {
+    switch_page6.addEventListener("click", () => {
+        window.location.href = "Shoulder_exercise.html";
+    });
+}
+
+
+const switch_page7 = document.querySelector(".first_meal");
+
+if (switch_page7) {
+    switch_page7.addEventListener("click", () => {
+        window.location.href = "Calories_Dense.html";
+    });
+}
+
+const switch_page8 = document.querySelector(".second_meal");
+
+if (switch_page8) {
+    switch_page8.addEventListener("click", () => {
+        window.location.href = "Calories_Burning.html";
+    });
+}
+
+const switch_page9 = document.querySelector(".third_meal");
+
+if (switch_page9) {
+    switch_page9.addEventListener("click", () => {
+        window.location.href = "Balanced_Meal.html";
+    });
+}
